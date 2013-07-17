824e86b38fbcf478e9baa76da27cb4d9b5b31440
Noirello <noirello@gmail.com>
2013-07-17 21:34:42 +0200

    Changed folder structure.

--- setup.py
+++ setup.py
@@ -1,8 +1,19 @@
+import os
 from distutils.core import setup, Extension
+
+sources = ["pyldapmodule.c", "ldapentry.c", 
+           "ldapclient.c", "ldapvaluelist.c", 
+           "errors.c", "utils.c", "uniquelist.c"]
+
+depends = ["errors.h", "ldapclient.h", "ldapvaluelist.h", "uniquelist.h", "utils.h"]
+
+sources = [ os.path.join('pyLDAP', x) for x in sources]
+depends = [ os.path.join('pyLDAP', x) for x in depends]
 
 pyldap_module = Extension("pyLDAP", 
             libraries=["ldap", "lber"],    
-            sources = ["pyldapmodule.c", "ldapentry.c", "ldapclient.c", "ldapvaluelist.c", "errors.c", "utils.c", "uniquelist.c"])
+            sources = sources,
+            depends = depends)
 
 setup(name="pyLDAP", 
       version="0.1.0",
