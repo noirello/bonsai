4bf56642bdce98181afa8b14541f30616885e981
noirello <noirello@gmail.com>
2013-08-10 12:49:10 +0200

    Added new tests to ldapentry_test.py.

--- tests/ldapentry_test.py
+++ tests/ldapentry_test.py
@@ -37,9 +37,31 @@ class LDAPEntryTest(unittest.TestCase):
         except:
             self.fail("Delete failed.")
             
+    def get_mail(self):
+        return self.entry['mail']
+    
+    def set_same(self):
+        self.entry['GivenName'].extend(['teSt', "test3"])
+    
+    def test_update(self):
+        self.entry.update({"GivenName": "test2", "mail" : "test@mail"})
+        self.entry.update([("sn", "test")])
+        self.assertEqual(self.entry['mail'], ['test@mail'])
+        self.assertEqual(self.entry['givenname'], ['test2'])
+        self.assertEqual(self.entry['sn'][0], 'test')
+            
     def test_ci(self):
         self.entry['givenName'] = "test"
+        self.entry['mail'] = "test@mail"
         self.assertEqual(self.entry['GiVenName'], self.entry['givenname'])
+        del self.entry['mAil']
+        self.assertRaises(KeyError, self.get_mail)
+        
+    def test_append(self):
+        self.entry['givenName'] =  "test"
+        self.entry['givenname'].append("test2")
+        self.assertEqual(self.entry['givenname'], ["test", "test2"])
+        self.assertRaises(TypeError, self.set_same)
         
 if __name__ == '__main__':
     unittest.main()
