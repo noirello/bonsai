b8ad64e3ce326585bab5570a59926afc85f1aee6
noirello <noirello@gmail.com>
2016-01-29 08:49:23 +0100

    Added Docker container files, configs, test data and test certs.

---
+++ tests/testenv/entrypoint.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+set -e
+
+# Check if the setup is already made. 
+[ -f /root/.setup ] && exit 0
+
+setDNS () {
+    LISTENINGIP=`ip -4 addr show scope global dev eth0 | grep inet | awk '{print \$2}' | cut -d / -f 1`
+
+    sed 's/{{HOSTNAME}}/'`hostname`'/g; s/{{HOSTIP}}/'$HOSTIP'/g; s/{{REVIP}}/'`echo $HOSTIP | awk -F. '{print $3"."$2"."$1}'`'/g; s/{{LISTENINGIP}}/'$LISTENINGIP'/g' < /root/named.conf.temp > /etc/named.conf
+    sed 's/{{HOSTNAME}}/'`hostname`'/g; s/{{HOSTIP}}/'$HOSTIP'/g' < /root/named.host.temp > /var/named/named.host
+    sed 's/{{HOSTNAME}}/'`hostname`'/g; s/{{HOSTIP}}/'$HOSTIP'/g; s/{{LASTDIGIT}}/'`echo $HOSTIP | awk -F. '{print $4}'`'/g' < /root/rev.host.temp > /var/named/rev.host
+}
+
+setKerberos () {
+    kdb5_util create -r BONSAI.TEST -s -W -P p@ssword
+    kadmin.local -q "addprinc -pw p@ssword admin"
+    kadmin.local -q "addprinc -pw p@ssword chuck"
+    kadmin.local -q "ktadd -k /var/kerberos/krb5kdc/kadm5.keytab kadmin/admin"
+    kadmin.local -q "ktadd -k /var/kerberos/krb5kdc/kadm5.keytab kadmin/changepw"
+    kadmin.local -q "addprinc -randkey host/bonsai.test"
+    kadmin.local -q "ktadd host/bonsai.test"
+    kadmin.local -q "add_principal -randkey ldap/bonsai.test"
+    kadmin.local -q "ktadd -keytab /etc/openldap/ldap.keytab ldap/bonsai.test"
+}
+
+# Load the LDIF files and some schema into the server.
+setLDAP () {
+    /usr/sbin/slapd -h "ldap:// ldapi:// ldaps://"
+    sleep 2
+    ldapmodify -Y EXTERNAL -H ldapi:/// -f /root/sasl.ldif
+    ldapadd -Y EXTERNAL -H ldapi:/// -f /etc/openldap/schema/cosine.ldif && ldapadd -Y EXTERNAL -H ldapi:/// -f /etc/openldap/schema/inetorgperson.ldif && ldapadd -Y EXTERNAL -H ldapi:/// -f /etc/openldap/schema/nis.ldif
+    ldapadd -x -D "cn=admin,dc=bonsai,dc=test" -w p@ssword -H ldapi:/// -f /root/base.ldif
+    # Allow VLV and server side sort.
+    ldapmodify -Y EXTERNAL -H ldapi:/// -f /root/sssvlv.ldif
+    # Stop the slapd. 
+    ps axf | grep /usr/sbin/slapd | grep -v grep | awk '{print "kill  " $1}'| sh
+}
+
+#Set passsword for SASL DIGEST-MD5.
+setDigest () {
+    echo "p@ssword" | saslpasswd2 -p root
+    echo "p@ssword" | saslpasswd2 -p chuck
+}
+
+setDNS
+setKerberos
+setDigest
+setLDAP
+
+touch /root/.setup
+
+exec "$@"
