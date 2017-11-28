a049653743613b8ca8ff1d926eb424be103b7768
noirello <noirello@gmail.com>
2017-11-28 22:46:50 +0100

    Add OpenLDAP configs for Mac OS

--- tests/testenv/generate_cert.sh
+++
@@ -1,3 +0,0 @@
-openssl genrsa -out server.key 2048
-openssl req -new -key server.key -out server.csr -subj "/C=XX/CN=bonsai.test"
-openssl x509 -req -days 500 -in server.csr -CA ./tests/testenv/certs/cacert.pem -CAkey ./tests/testenv/certs/cacert.key -CAcreateserial -out server.pem -sha256
