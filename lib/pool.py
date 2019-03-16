3a2e1c5b61324a17bef4363182725cd307620385
noirello <noirello@gmail.com>
2019-03-16 22:22:56 +0100

    Add ThreadedConnectionPool implementation

--- lib/pool.py
+++ lib/pool.py
@@ -1,3 +1,6 @@
+import threading
+from typing import Optional
+
 from .ldapclient import LDAPClient
 
 
@@ -22,24 +25,25 @@ class ConnectionPool:
         self._minconn = minconn
         self._maxconn = maxconn
         self._client = client
-        self._closed = False
+        self._closed = True
         self._idles = set()
         self._used = set()
 
     def open(self):
         for _ in range(self._minconn):
             self._idles.add(self._client.connect())
+        self._closed = False
 
     def get(self):
         if self._closed:
             raise ClosedPool("The pool is closed.")
         try:
             conn = self._idles.pop()
-        except IndexError:
+        except KeyError:
             if len(self._used) < self._maxconn:
                 conn = self._client.connect()
             else:
-                raise EmptyPool("Pool is empty.")
+                raise EmptyPool("Pool is empty.") from None
         self._used.add(conn)
         return conn
 
@@ -52,9 +56,55 @@ class ConnectionPool:
         except KeyError:
             raise PoolError("The %r is not managed by this pool." % conn) from None
 
+    @property
+    def empty(self):
+        return len(self._idles) == 0 and len(self._used) == self._maxconn
+
     def close(self):
         for conn in self._idles:
             conn.close()
         for conn in self._used:
             conn.close()
         self._closed = True
+        self._idles = set()
+        self._used = set()
+
+
+class ThreadedConnectionPool(ConnectionPool):
+    def __init__(
+        self,
+        client: LDAPClient,
+        minconn: int = 1,
+        maxconn: int = 10,
+        block: bool = True,
+    ):
+        super().__init__(client, minconn, maxconn)
+        self._block = block
+        self._lock = threading.Condition()
+
+    def get(self, timeout: Optional[float] = None):
+        self._lock.acquire()
+        try:
+            if self._block:
+                self._lock.wait_for(lambda: not self.empty or self._closed, timeout)
+            conn = super().get()
+            self._lock.notify()
+            return conn
+        finally:
+            self._lock.release()
+
+    def put(self, conn):
+        self._lock.acquire()
+        try:
+            super().put(conn)
+            self._lock.notify()
+        finally:
+            self._lock.release()
+
+    def close(self):
+        self._lock.acquire()
+        try:
+            super().close()
+            self._lock.notify_all()
+        finally:
+            self._lock.release()
