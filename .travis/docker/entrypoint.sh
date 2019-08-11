9c63258090581e1d6c712169b462d5abd1c35d46
noirello <noirello@gmail.com>
2019-08-11 22:49:49 +0200

    Make entrypoint.sh and start_slapd.sh to Debian compatible

--- .travis/docker/entrypoint.sh
+++ .travis/docker/entrypoint.sh
@@ -9,29 +9,29 @@ setKerberos () {
     kdb5_util create -r BONSAI.TEST -s -W -P p@ssword
     kadmin.local -q "addprinc -pw p@ssword admin"
     kadmin.local -q "addprinc -pw p@ssword chuck"
-    kadmin.local -q "ktadd -k /var/kerberos/krb5kdc/kadm5.keytab kadmin/admin"
-    kadmin.local -q "ktadd -k /var/kerberos/krb5kdc/kadm5.keytab kadmin/changepw"
+    kadmin.local -q "ktadd -k /etc/krb5kdc/kadm5.keytab kadmin/admin"
+    kadmin.local -q "ktadd -k /ect/krb5kdc/kadm5.keytab kadmin/changepw"
     kadmin.local -q "addprinc -randkey host/bonsai.test"
     kadmin.local -q "ktadd host/bonsai.test"
     kadmin.local -q "add_principal -randkey ldap/bonsai.test"
-    kadmin.local -q "ktadd -keytab /etc/openldap/ldap.keytab ldap/bonsai.test"
-    chown ldap:ldap /etc/openldap/ldap.keytab
+    kadmin.local -q "ktadd -keytab /etc/ldap/ldap.keytab ldap/bonsai.test"
+    chown ldap:ldap /etc/ldap/ldap.keytab
     chown ldap:ldap /var/log/kadmin.log
-    chown -Rf ldap:ldap /var/kerberos/krb5kdc/
+    chown -Rf ldap:ldap /etc/krb5kdc/
+    chown -Rf ldap:ldap /var/lib/krb5kdc/
     setcap 'cap_net_bind_service=+ep' /usr/sbin/krb5kdc # Allow to open port
     setcap 'cap_net_bind_service=+ep' /usr/sbin/kadmind
 }
 
 # Load the LDIF files and some schema into the server.
 setLDAP () {
+    chown -Rf ldap:ldap /var/run/slapd
+    chmod 500 /etc/ldap/certs/server.pem
+    chmod 500 /etc/ldap/certs/server.key
     /usr/sbin/slapd -u ldap -g ldap -h "ldap:// ldapi:// ldaps://"
     sleep 2
     ldapmodify -Y EXTERNAL -H ldapi:/// -f /home/ldap/settings.ldif
-    # Load schemas.
-    ldapadd -Y EXTERNAL -H ldapi:/// -f /etc/openldap/schema/cosine.ldif
-    ldapadd -Y EXTERNAL -H ldapi:/// -f /etc/openldap/schema/inetorgperson.ldif
-    ldapadd -Y EXTERNAL -H ldapi:/// -f /etc/openldap/schema/nis.ldif
-    ldapadd -Y EXTERNAL -H ldapi:/// -f /etc/openldap/schema/ppolicy.ldif
+    ldapadd -Y EXTERNAL -H ldapi:/// -f /etc/ldap/schema/ppolicy.ldif
     ldapmodify -Y EXTERNAL -H ldapi:/// -f /home/ldap/schema.ldif
     # Set overlays: allow vlv, server side sort and password policy.
     ldapmodify -Y EXTERNAL -H ldapi:/// -f /home/ldap/overlays.ldif
@@ -44,6 +44,8 @@ setLDAP () {
     # Stop the slapd. 
     ps axf | grep /usr/sbin/slapd | grep -v grep | awk '{print "kill  " $1}'| sh
     setcap 'cap_net_bind_service=+ep' /usr/sbin/slapd
+    # Remove SCRAM.
+    rm -rf /usr/lib/x86_64-linux-gnu/sasl2/libscram.*
 }
 
 #Set passsword for SASL DIGEST-MD5.
