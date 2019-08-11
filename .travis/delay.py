592190f4d2337c45a1cde5a043334da339e56923
noirello <noirello@gmail.com>
2019-08-11 22:52:20 +0200

    Fix getting network interface for delay on Debian, run format

--- .travis/delay.py
+++ .travis/delay.py
@@ -10,32 +10,71 @@ try:
 except ImportError:
     pass
 
-class LinuxDelayHandler:
 
+class LinuxDelayHandler:
     @staticmethod
     def get_interface_name():
         """ Get the first interface name that is not the localhost. """
         net = os.listdir("/sys/class/net")
         net.remove("lo")
+        if "eth0" in net:
+            return "eth0"
         return net[0]
 
     def set_delay(self, sec, duration=10.0):
         """ Set network delay, return with the call's result. """
         try:
-            subprocess.check_call(["tc", "qdisc", "add", "dev",
-                                   self.get_interface_name(), "root",
-                                   "handle", "1:", "prio"])
-            subprocess.check_call(["tc", "qdisc", "add", "dev",
-                                   self.get_interface_name(), "parent",
-                                   "1:3", "handle", "30:", "netem",
-                                   "delay", ("%dmsec" % (sec * 1000))])
+            subprocess.check_call(
+                [
+                    "tc",
+                    "qdisc",
+                    "add",
+                    "dev",
+                    self.get_interface_name(),
+                    "root",
+                    "handle",
+                    "1:",
+                    "prio",
+                ]
+            )
+            subprocess.check_call(
+                [
+                    "tc",
+                    "qdisc",
+                    "add",
+                    "dev",
+                    self.get_interface_name(),
+                    "parent",
+                    "1:3",
+                    "handle",
+                    "30:",
+                    "netem",
+                    "delay",
+                    ("%dmsec" % (sec * 1000)),
+                ]
+            )
             for port in ("389", "636"):
-                subprocess.check_call(["tc", "filter", "add",
-                                       "dev", self.get_interface_name(),
-                                       "protocol", "ip", "parent",
-                                       "1:0", "u32", "match", "ip",
-                                       "sport", port, "0xffff", "flowid",
-                                       "1:3"])
+                subprocess.check_call(
+                    [
+                        "tc",
+                        "filter",
+                        "add",
+                        "dev",
+                        self.get_interface_name(),
+                        "protocol",
+                        "ip",
+                        "parent",
+                        "1:0",
+                        "u32",
+                        "match",
+                        "ip",
+                        "sport",
+                        port,
+                        "0xffff",
+                        "flowid",
+                        "1:3",
+                    ]
+                )
 
             return True
         except subprocess.CalledProcessError:
@@ -44,8 +83,9 @@ class LinuxDelayHandler:
     def remove_delay(self):
         """ Remove network delay. """
         try:
-            subprocess.check_call(["tc", "qdisc", "del", "dev",
-                                   self.get_interface_name(), "root"])
+            subprocess.check_call(
+                ["tc", "qdisc", "del", "dev", self.get_interface_name(), "root"]
+            )
             return True
         except subprocess.CalledProcessError:
             return False
@@ -53,17 +93,26 @@ class LinuxDelayHandler:
 
 class MacDelayHandler:
     def set_delay(self, sec, duration=10.0):
-        with open('/etc/pf.conf') as fp:
+        with open("/etc/pf.conf") as fp:
             conf = fp.read()
             conf += '\ndummynet-anchor "mop"\nanchor "mop"\n'
-            rule = "dummynet in quick proto tcp from any to any port {389, 636} pipe 1\n"
+            rule = (
+                "dummynet in quick proto tcp from any to any port {389, 636} pipe 1\n"
+            )
 
             try:
-                subprocess.run(["pfctl", "-f", "-"], input=conf, encoding="utf-8", check=True)
-                subprocess.run(["pfctl", "-a", "mop", "-f", "-"], input=rule,
-                               encoding="utf-8", check=True)
-                subprocess.check_call(["dnctl", "pipe", "1", "config", "delay",
-                                       "%d" % int(sec * 1000)])
+                subprocess.run(
+                    ["pfctl", "-f", "-"], input=conf, encoding="utf-8", check=True
+                )
+                subprocess.run(
+                    ["pfctl", "-a", "mop", "-f", "-"],
+                    input=rule,
+                    encoding="utf-8",
+                    check=True,
+                )
+                subprocess.check_call(
+                    ["dnctl", "pipe", "1", "config", "delay", "%d" % int(sec * 1000)]
+                )
 
                 return True
             except subprocess.CalledProcessError:
@@ -76,6 +125,7 @@ class MacDelayHandler:
             return True
         except subprocess.CalledProcessError:
             return False
+
 
 class WinDelayHandler:
     proc = None
@@ -102,6 +152,7 @@ class WinDelayHandler:
             self.proc.terminate()
         return True
 
+
 if __name__ == "__main__":
     if sys.platform == "win32":
         handler = WinDelayHandler()
@@ -110,8 +161,9 @@ if __name__ == "__main__":
     else:
         handler = LinuxDelayHandler()
         # Fix network collapse on certain Linux distros.
-        subprocess.call(["ip", "link", "set", handler.get_interface_name(),
-                         "qlen", "1000"])
+        subprocess.call(
+            ["ip", "link", "set", handler.get_interface_name(), "qlen", "1000"]
+        )
 
     server = rpc.SimpleXMLRPCServer(("0.0.0.0", 8000))
     server.register_function(handler.set_delay, "set_delay")
