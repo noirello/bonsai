61b276356da453d9d9755b9b1fa449389cd3d955
noirello <noirello@gmail.com>
2021-08-01 23:39:43 +0200

    Fix test_referral_chasing Add test_ignore_referrals

--- tests/test_ldapreference.py
+++ tests/test_ldapreference.py
@@ -16,7 +16,7 @@ def host_url():
     curdir = os.path.abspath(os.path.dirname(__file__))
     cfg = configparser.ConfigParser()
     cfg.read(os.path.join(curdir, "test.ini"))
-    url = "ldap://%s:%s" % (cfg["SERVER"]["hostip"], cfg["SERVER"]["port"])
+    url = "ldap://%s:%s" % (cfg["SERVER"]["hostname"], cfg["SERVER"]["port"])
     return url
 
 
@@ -60,13 +60,33 @@ def test_referral_chasing(host_url):
     """ Testing referral chasing option. """
     refdn = "o=admin,ou=nerdherd-refs,dc=bonsai,dc=test"
     client = LDAPClient(host_url)
+    client.server_chase_referrals = True
     with client.connect() as conn:
         res = conn.search(refdn, 0)
         assert isinstance(res[0], bonsai.LDAPEntry)
     client.server_chase_referrals = False
+    client.ignore_referrals = False
     with client.connect() as conn:
         res = conn.search(refdn, 0)
         assert len(res) == 0
         res = conn.search("ou=nerdherd-refs,dc=bonsai,dc=test", 1)
+        refs = [item for item in res if isinstance(item, LDAPReference)]
+        assert any(refs)
+
+
+@pytest.mark.skipif(
+    sys.platform.startswith("win"), reason="Referrals are not set in AD."
+)
+def test_ignore_referrals(host_url):
+    """ Testing ignore referrals option. """
+    client = LDAPClient(host_url)
+    client.ignore_referrals = True
+    with client.connect() as conn:
+        res = conn.search("ou=nerdherd-refs,dc=bonsai,dc=test", 1)
+        assert res == []
+    client.ignore_referrals = False
+    with client.connect() as conn:
+        res = conn.search("ou=nerdherd-refs,dc=bonsai,dc=test", 1)
+        assert len(res) != 0
         refs = [item for item in res if isinstance(item, LDAPReference)]
         assert any(refs)
