88fd31771327830a31de11f2aa071f15ff0a0acc
noirello <noirello@gmail.com>
2023-09-08 01:08:40 +0200

    Update year, better late than never

--- docs/conf.py
+++ docs/conf.py
@@ -81,7 +81,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'bonsai'
-copyright = '2014-2022, noirello'
+copyright = '2014-2023, noirello'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
